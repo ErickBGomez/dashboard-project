d00de2f1a2c9e5b229e7dff84f8ba8cd6e7d670b
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-04-26 12:03:06 -0600

    Change trending posts count with randomize function

--- js/randomize-values.js
+++ js/randomize-values.js
@@ -1,4 +1,5 @@
 const viewCounts = document.querySelectorAll(".view-count");
+const trendingPostsCounts = document.querySelectorAll(".trending-posts-count");
 
 // Random number between two values (both inclusive)
 function getRandomNumberRange(minValue, maxValue) {
@@ -7,6 +8,12 @@ function getRandomNumberRange(minValue, maxValue) {
     return Math.floor(Math.random() * (maxValue - minValue + 1) + minValue);
 }
 
+// Randomize view count to posts elements
 viewCounts.forEach(viewCount => {
-    viewCount.innerText = getRandomNumberRange(0, 999) + "K";
+    viewCount.innerText = getRandomNumberRange(1, 999) + "K";
+});
+
+// Randomize trending posts counts
+trendingPostsCounts.forEach(postCount => {
+    postCount.innerText = getRandomNumberRange(1, 999) + "K";
 });
