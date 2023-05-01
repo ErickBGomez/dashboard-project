43bbb12898fe6f618ae5f0ddb7f29d6040f42501
Erick B. Gómez <90425287+ErickBGomez@users.noreply.github.com>
2023-05-01 12:45:14 -0600

    Change greetings text based on local time

--- js/getTime.js
+++ js/getTime.js
@@ -1,2 +1,21 @@
+const morning = 6;
+const afternoon = 12;
+const evening = 17;
+const night = 22;
+
 const localHour = new Date().getHours();
-console.log(localHour);
+const greetingsElement = document.querySelector(".greeting-time");
+
+function setGreetingsText() {
+    if (localHour >= morning && localHour < afternoon) {
+        greetingsElement.innerText = "morning";
+    } else if (localHour >= afternoon && localHour < evening) {
+        greetingsElement.innerText = "afternoon";
+    } else if (localHour >= evening && localHour < night) {
+        greetingsElement.innerText = "evening";
+    } else {
+        greetingsElement.innerText = "night";
+    }
+}
+
+setGreetingsText();
